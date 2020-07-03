f5502e80c43a70b03ff9065a5fd8dab95128111e
Kasinerava <65457233+Kasinerava@users.noreply.github.com>
2020-07-04 01:21:47 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+<script>
+
+	let user_enter=document.querySelector("url-enter");
+	let modal_login=document.querySelector("modal-login");
+
+	link.addEventListener("click" funcrion(evt)
+	{
+		evt.preventDefault();
+		modal_login.classList.toggle("login-show");
+		modal_login.classList.toggle("modal-login");
+	})
+
+</script>
