70c0ae3aecf5ef13a13cd1f84f5513091e531f01
Kasinerava <65457233+Kasinerava@users.noreply.github.com>
2020-07-05 18:24:57 +0300

    1

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
-	let user_enter=document.querySelector("url-enter");
-	let modal_login=document.querySelector("modal-login");
+let popup = document.querySelector(".modal-feedback");
+let overlay = document.querySelector(".overlay");
+let close = document.querySelector(".button-close");
 
-	user_enter.addEventListener("click" function(evt)
-	{
-		evt.preventDefault();
-		modal_login.classList.toggle("login-show");
-		modal_login.classList.toggle("modal_login");
-	});
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-feedback-show");
+  overlay.classList.add("overlay-show");
+  if (nameStorage && emailStorage) {
+    name.value = nameStorage;
+    email.value = emailStorage;
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-feedback-show");
+  overlay.classList.remove("overlay-show");
+});
